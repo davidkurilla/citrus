c2e878bad81857da740c7a42085aa7cd7bbb0888
David Kurilla <davidkurilla03@gmail.com>
2024-05-16 13:17:59 -0700

    add list and delete commands

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,33 @@ fn main() {
             SubCommand::with_name("run")
                 .about("Run a task")
         )
-	.subcommand(
-	    SubCommand::with_name("create")
-		.about("Create a task")
-	)
+	    .subcommand(
+	        SubCommand::with_name("create")
+		    .about("Create a task")
+	    )
+        .subcommand(
+            SubCommand::with_name("list")
+            .about("List tasks")
+        )
+        .subcommand(
+            SubCommand::with_name("delete")
+            .about("Deleete a task")
+        )
         .get_matches();
 
     match matches.subcommand_name() {
         Some("run") => {
             println!("Running task...");
         }
-	Some("create") => {
-	    println!("Creating task...");
-	}
+	    Some("create") => {
+	        println!("Creating task...");
+	    }
+        Some("list") => {
+            println!("Listing tasks...");
+        }
+        Some("delete") => {
+            println!("Deleting task...");
+        }
         _ => {
             println!("Welcome to Citrus!");
         }
